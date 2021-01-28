be0ff8ee380130a1ea04eb419e976dd08c0af8d9
Reza Aghaei <r.aghaei@outlook.com>
2021-01-28 20:49:33 +0800

    Added service layer.

---
+++ ServiceLayer/MyWCFService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLayer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyWCFService" in both code and config file together.
+    public class MyWCFService : IMyWCFService
+    {
+        public string Echo(string message)
+        {
+            return message;
+        }
+    }
+}
